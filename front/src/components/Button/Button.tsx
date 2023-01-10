6194d7199ecc24919a8ce24584ec975656f9373e
mariadriada <mariagiraldo4@gmail.com>
2023-01-10 02:19:02 -0500

    feat [front]: add sass support

--- front/src/components/Button/Button.tsx
+++ front/src/components/Button/Button.tsx
@@ -1,11 +1,11 @@
 import React, {FC} from "react"
 
 import { useGlobalStore } from "../../redux-toolkit/store"
+import "./styles.scss"
 
 const Button:FC = ()=> {
-
     const {testMessage} = useGlobalStore()
-    return<div>{testMessage}</div>
+    return<div className="button">{testMessage}</div>
 }
 
 export default Button
