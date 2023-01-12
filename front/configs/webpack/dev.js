52ba3479d6f26687aca9bf2948c11882ea02ac2b
mariadriada <mariagiraldo4@gmail.com>
2023-01-11 20:46:09 -0500

    feat [front]: add webpack and babel config

---
+++ front/configs/webpack/dev.js
@@ -0,0 +1,16 @@
+// development config
+const { merge } = require("webpack-merge");
+const commonConfig = require("./common");
+const ReactRefreshPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+
+module.exports = merge(commonConfig, {
+  mode: "development",
+  devServer: {
+    hot: true,
+    historyApiFallback: true, 
+    port: 3000,
+  },
+  devtool: "cheap-module-source-map",
+  plugins: [new ReactRefreshPlugin()],
+});
+
